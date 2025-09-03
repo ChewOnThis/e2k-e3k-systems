2e2f1cc72e9a345c8ce9ef4ceb278f8feb5db499
Cardinal Cin <caleb.chew05@gmail.com>
2025-09-03 16:14:54 +1000

    Emergency functions

--- psuedocode/ControlBridge/EmergencyLower.h
+++ psuedocode/ControlBridge/EmergencyLower.h
@@ -2,6 +2,34 @@
 #include <Arduino.h>
 #include "config.h"
 
-namespace EmergencyLower{
+namespace EmergencyLower {
+  enum class Result { STAY, TO_DOWN };
 
-} 
+  // extern helpers
+  extern void roadRed();
+  extern void marineRed();
+  extern void gatesDown();
+  extern void motorEnable();
+  extern void motorDirDown();
+  extern void motorCruiseDown();
+  extern void motorStop();
+  extern bool bottomLimitPressed();
+
+  static bool s_inited = false;
+
+  inline void enter() {
+    s_inited = true;
+    roadRed(); marineRed(); gatesDown();
+    motorEnable(); motorDirDown(); motorCruiseDown();
+  }
+
+  inline Result tick() {
+    if (!s_inited) enter();
+
+    if (bottomLimitPressed()) {
+      motorStop();
+      return Result::TO_DOWN;
+    }
+    return Result::STAY;
+  }
+}
--- psuedocode/ControlBridge/EmergencyRaise.h
+++ psuedocode/ControlBridge/EmergencyRaise.h
@@ -2,6 +2,34 @@
 #include <Arduino.h>
 #include "config.h"
 
-namespace EmergencyRaise{
+namespace EmergencyRaise {
+  enum class Result { STAY, TO_UP };
 
+  // extern helpers
+  extern void roadRed();
+  extern void marineRed();
+  extern void gatesDown();
+  extern void motorEnable();
+  extern void motorDirUp();
+  extern void motorCruiseUp();
+  extern void motorStop();
+  extern bool topLimitPressed();
+
+  static bool s_inited = false;
+
+  inline void enter() {
+    s_inited = true;
+    roadRed(); marineRed(); gatesDown();
+    motorEnable(); motorDirUp(); motorCruiseUp();
+  }
+
+  inline Result tick() {
+    if (!s_inited) enter();
+
+    if (topLimitPressed()) {
+      motorStop();
+      return Result::TO_UP;
+    }
+    return Result::STAY;
+  }
 }
