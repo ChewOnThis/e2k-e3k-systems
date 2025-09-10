94599cc14a1573c39defc7971bcb17cefe5bd879
Aidan-Williams <aidan.williams1@students.mq.edu.au>
2025-09-10 14:33:37 +1000

    aidan stuff

---
+++ testing/Double_Ultrasonic_V2/Double_Ultrasonic_V2.ino
@@ -0,0 +1,78 @@
+/* Credit:
+ * https://arduino.stackexchange.com/questions/36942/managing-2-ultrasonic-sensors-without-blocking-the-other
+*/
+
+volatile unsigned long LastPulseTimeA;
+volatile unsigned long LastPulseTimeB;
+int durationA;
+int durationB;
+//unsigned long startTime;
+#define trigPinA 12
+#define echoPinA 2
+#define trigPinB 13
+#define echoPinB 3
+
+
+
+void setup() {
+  Serial.begin (115200);
+  pinMode(trigPinA, OUTPUT);
+  pinMode(echoPinA, INPUT);
+  pinMode(trigPinB, OUTPUT);
+  pinMode(echoPinB, INPUT);  
+	attachInterrupt(digitalPinToInterrupt(echoPinA), EchoPinA_ISR, CHANGE);  // Pin 2 interrupt on any change
+	attachInterrupt(digitalPinToInterrupt(echoPinB),EchoPinB_ISR, CHANGE);  // Pin3 interrupt on any change
+}
+
+
+
+void loop(){
+  digitalWrite(trigPinA, LOW);
+  digitalWrite(trigPinB, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPinA, HIGH);
+  digitalWrite(trigPinB, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPinA, LOW);
+  digitalWrite(trigPinB, LOW);
+  Serial.print("SensorA:");
+  // Serial.print(LastPulseTimeA);
+  // Serial.print('\t');
+  Serial.print((LastPulseTimeA/2) / 29.1,1);
+  // Serial.println("cm");
+	Serial.println();
+  Serial.print("SensorB:");
+  // Serial.print(LastPulseTimeB);
+  // Serial.print('\t');
+  Serial.print((LastPulseTimeB/2) / 29.1,1);
+  // Serial.println("cm");
+	Serial.println();
+
+  delay(100);
+}
+
+
+
+void EchoPinA_ISR() {
+    static unsigned long startTimeA;
+
+    if (digitalRead(echoPinA)) // Gone HIGH
+        startTimeA = micros();
+    else  // Gone LOW
+        LastPulseTimeA = micros() - startTimeA;
+}
+
+
+
+void EchoPinB_ISR() {
+    static unsigned long startTimeB;
+
+    if (digitalRead(echoPinB)) // Gone HIGH
+        startTimeB = micros();
+    else  // Gone LOW
+        LastPulseTimeB = micros() - startTimeB;
+}
+
+
+
+
